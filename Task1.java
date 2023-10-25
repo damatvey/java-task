89b55b8776ab755b56c0169864498ae31cec7dcc
damatvey <damatveywork@gmail.com>
2023-10-25 19:46:32 +0300

    fixed a bug

--- Task1.java
+++ Task1.java
@@ -5,6 +5,10 @@ public class Task1 {
         Scanner scan = new Scanner(System.in);
         String stroka = scan.nextLine();
         int n = scan.nextInt();
+        if (n <= 0){
+            System.out.println(stroka);
+            return;
+        }
         for (int i = 0; i < n; i++) {
             System.out.println(stroka);
         }
